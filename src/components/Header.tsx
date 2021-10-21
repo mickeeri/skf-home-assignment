f617d7b458e0f84e2f20190e7c2f45e94ebf9793
Mikael Eriksson <micke_eri@hotmail.com>
2021-10-21 19:11:26 +0200

    Change the title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 const Header = () => {
   return (
     <header className="fixed top-0 w-full h-12 flex items-center bg-black">
-      <h1 className="text-white ml-8 font-bold text-2xl">My App</h1>
+      <h1 className="text-white ml-8 font-bold text-2xl">
+        SKF Home Assignment
+      </h1>
     </header>
   );
 };
